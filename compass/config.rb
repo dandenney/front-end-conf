78caba4c0348232338eaf390c751ce2298a2258b
dandenney <dd.subscriptions@gmail.com>
2013-03-19 23:44:43 -0400

    Borked the original repo, initial commit on the new one

---
+++ compass/config.rb
@@ -0,0 +1,16 @@
+# Compass configuration file.
+
+# Please read the docs http://docs.mixture.io/preprocessors#compass, we will soon support
+# the ability to easily require plugins.
+
+project_path = "/Users/dandenney/Documents/repos/dandenney/front-end-conf/"
+
+# Important! change the paths below to match your project setup
+css_dir = "public/styles" # update to the path of your css files.
+sass_dir = "public/styles/sass" # update to the path of your sass files.
+images_dir = "public/images" # update to the path of your image files.
+javascripts_dir = "public/scripts" # update to the path of your script files.
+
+line_comments = true # if debugging (or using Mixture chrome extension - set this to true)
+cache = true
+color_output = false # required for Mixture
