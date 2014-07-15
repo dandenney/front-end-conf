c9264343bf7027edef68b8eebb946c6a8f1f26ef
dandenney <dd.subscriptions@gmail.com>
2014-07-14 22:06:39 -0400

    Adds lots of stuff in my haphazard fashion

---
+++ _site/README.md
@@ -0,0 +1,4 @@
+front-end-conf
+==============
+
+Getting the versioning juices running for the new version of frontendconf.com. It is created and hosted via Mixture.io.
---
+++ _site/compass/config.rb
@@ -0,0 +1,17 @@
+# Compass configuration file.
+
+# Please read the docs http://docs.mixture.io/preprocessors#compass, we will soon support
+# the ability to easily require plugins.
+
+project_path = File.expand_path("..",File.dirname(__FILE__))
+
+# Important! change the paths below to match your project setup
+css_dir = "public/styles" # update to the path of your css files.
+sass_dir = "public/styles/sass" # update to the path of your sass files.
+images_dir = "public/images" # update to the path of your image files.
+javascripts_dir = "public/scripts" # update to the path of your script files.
+relative_assets = true
+
+line_comments = true # if debugging (or using Mixture chrome extension - set this to true)
+cache = true
+color_output = false # required for Mixture
